27e6a4b8920c70673fa045811c058e1a7b5cd481
makifcevik <makif.cevik02@gmail.com>
2025-02-05 12:04:30 +0300

    quotes-app commit 2

--- 9-quotes-app/src/App.jsx
+++ 9-quotes-app/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     const fetchQuotes = async () => {
       try {
         const response = await axios.get(API_ENDPOINT, {
-          headers: {
+          params: {
             limit: 0
           }
         });
